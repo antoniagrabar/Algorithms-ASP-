2efbcc738fb9e765e37756841d014429d2668b83
Antonia <62219941+antoniagrabar@users.noreply.github.com>
2020-10-26 11:25:55 +0100

    Stack, linked list

---
+++ TopTopovaLista.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef DEBUG
+#define DEBUG(...) printf(__VA_ARGS__)
+#endif
+
+typedef struct Node {
+    struct Node *next;
+    int val;
+} node;
+
+int push(node **top, int val) {
+    node *new;
+    new = (node *)malloc(sizeof(node));
+    if (!new) return -1;
+
+    new->val = val;
+    new->next = *top;
+    *top = new;
+
+    return 0;
+}
+
+int pop(node **top) {
+    node *t;
+
+    if(!*top) return -1;
+
+    DEBUG("pop: %d", (*top)->val);
+
+    t = (*top)->next;
+    free(*top);
+    *top = t;
+
+    return 0;
+}
+
+int print(node *top) {
+    node *t;
+
+    if(!top) return -1;
+
+    for(t=top; t!=NULL; t=t->next) printf("%d ", t->val);
+
+    return 0;
+
+}
+
+/*
+void ime_funkcije(argumenti) {
+}
+*/
+
+int main() {
+    node *top = NULL;
+    int menu_choice, val, ret_val;
+    char c;
+
+    setbuf(stdout, NULL);
+    do {
+        menu_choice = 0;
+        //DEBUG("\n1 opis funkcije (ime_funkcije)");
+        DEBUG("\n2 dodaj (push)\n3 brisi (pop)\n4 ispis (print)\n5 izlaz\n");
+        scanf("%d", &menu_choice);
+        switch (menu_choice) {
+            case 1:
+                //ime_funkcije(argumenti);
+                break;
+            case 2:
+                scanf("%d", &val);
+                ret_val = push(&top, val);
+                if(ret_val==-1) DEBUG("Alokacija nije uspjela.");
+                break;
+            case 3:
+                ret_val = pop(&top);
+                if(ret_val==-1) DEBUG("Stog je prazan.");
+                break;
+            case 4:
+                ret_val = print(top);
+                if(ret_val==-1) DEBUG("Stog je prazan.");
+                break;
+            case 5:
+                break;
+            default:
+                while((c = getchar()) != '\n' && c != EOF);
+        }
+    } while(menu_choice!=5);
+
+    return 0;
+}
